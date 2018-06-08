dabd526fdbe889854a25c79e77cba3744ccc8069
Johnny M. Almeida <johnny.almeida@4all.com>
2018-06-08 10:27:23 -0300

    Implement post video test

--- test/integration/routes/video.js
+++ test/integration/routes/video.js
@@ -1,10 +1,24 @@
-describe('Route Vide', () => {
+describe('Route Video', () => {
   describe('Route GET /video', () => {
     it('should return status 200', (done) => {
       request
         .get('/video')
         .end((err, res) => {
           expect(res).to.have.status(200);
+          done(err);
+        });
+    });
+  });
+
+  describe('Route POST /video', () => {
+    it('should upload a file', (done) => {
+      request
+        .post('/api/uploads')
+        .field('test', 'myTest')
+        .attach('files', './_files/sample_video.mp4', 'sample_video.mp4')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body[0].location).to.include('sample_video.mp4');
           done(err);
         });
     });
