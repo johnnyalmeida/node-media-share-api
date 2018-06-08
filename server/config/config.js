577c3fdfbdf8a7112787472762a0c7932ddf20c2
Johnny M. Almeida <johnny.almeida@4all.com>
2018-06-08 09:09:04 -0300

    use dotenv to manage local config

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,11 @@
-export default {
-  aws_key: '',
-  aws_secret: 'asdasda',
-  port: 3000,
+import env from 'dotenv';
+
+
+export default () => {
+  env.config();
+  return {
+    aws_key: process.env.AWS_KEY,
+    aws_secret: process.env.AWS_SECRET,
+    port: 3000,
+  };
 };
