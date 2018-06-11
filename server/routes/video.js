0e60118a897921b0d7cd45d06705985a2ebad373
Johnny M. Almeida <johnny.almeida@4all.com>
2018-06-11 14:08:31 -0300

    Get video files direct from s3 and stream

--- server/routes/video.js
+++ server/routes/video.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import AWS from 'aws-sdk';
-
 import VideoController from '../controllers/video';
 
 export default (app) => {
@@ -9,6 +8,63 @@ export default (app) => {
   app.get('/', (req, res) => {
     fs.readFile('./_files/index.html', (err, html) => res.end(html));
   });
+
+  app.route('/video/stream/:key')
+    .get((req, res) => {
+      const { key } = req.params;
+
+      AWS.config.update({
+        accessKeyId: app.config.aws_key,
+        secretAccessKey: app.config.aws_secret,
+      });
+
+      const params = {
+        Bucket: app.config.aws_bucket,
+        Key: `processed/${key}`,
+      };
+
+      const s3 = new AWS.S3();
+
+      s3.listObjectsV2({ Bucket: params.Bucket, MaxKeys: 1, Prefix: `processed/${key}` }, (err, data) => {
+        if (err) {
+          return res.sendStatus(404);
+        }
+        if (req != null && req.headers.range != null) {
+          const { range } = req.headers;
+          const bytes = range.replace(/bytes=/, '').split('-');
+          const start = parseInt(bytes[0], 10);
+
+          const total = data.Contents[0].Size;
+          const end = bytes[1] ? parseInt(bytes[1], 10) : total - 1;
+          const chunksize = (end - start) + 1;
+
+          res.writeHead(206, {
+            'Content-Range': `bytes ${start}-${end}/${total}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Last-Modified': data.Contents[0].LastModified,
+            'Content-Type': 'video/mp4',
+          });
+          s3.getObject({ Bucket: params.Bucket, Key: `processed/${key}`, Range: range })
+            .createReadStream()
+            .pipe(res);
+        } else {
+          const cache = 3600;
+          res.writeHead(
+            200,
+            {
+              'Cache-Control': `max-age=${cache}, private`,
+              'Content-Length': data.Contents[0].Size,
+              'Last-Modified': data.Contents[0].LastModified,
+              'Content-Type': 'video/mp4',
+            },
+          );
+          s3.getObject(params).createReadStream().pipe(res);
+
+          return res.status(206);
+        }
+      });
+    });
 
   app.route('/video/:key')
     .get((req, res) => {
@@ -58,41 +114,6 @@ export default (app) => {
             return res.status(206);
           });
         });
-    });
-
-  app.route('/old/video/:key')
-    .get((req, res) => {
-      const { key } = req.params;
-      const movieFile = `${app.config.aws_bucket}/processed/${key}`;
-
-      console.log(movieFile);
-
-      fs.stat(movieFile, (err, stats) => {
-        if (err) {
-          return res.status(404).json(err);
-        }
-        // Setup the chunk headers variables
-        const { range } = req.headers;
-        const { size } = stats;
-        const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
-        const end = size - 1;
-        const chunkSize = (end - start) + 1;
-        // Set chunk headers
-        res.set({
-          'Content-Range': `bytes ${start}-${end}/${size}`,
-          'Accept-Ranges': 'bytes',
-          'Content-Length': chunkSize,
-          'Content-Type': 'video/mp4',
-        });
-
-        // Read the file and send chunks through stream.pipe()
-        const stream = fs.createReadStream(movieFile, { start, end });
-        stream.on('open', () => stream.pipe(res));
-        stream.on('error', streamErr => res.end(streamErr));
-
-        // Send a PARTIAL CONTENT status code
-        return res.status(206);
-      });
     });
 
   app.route('/video')
