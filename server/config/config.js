71b4db6f71054e6620192e3b30eeda3960b2135b
Johnny M. Almeida <johnny.almeida@4all.com>
2018-06-11 11:59:23 -0300

    fix loading video from S3 to stream

--- server/config/config.js
+++ server/config/config.js
@@ -7,5 +7,6 @@ export default {
   aws_key: process.env.AWS_KEY,
   aws_secret: process.env.AWS_SECRET,
   aws_bucket: process.env.AWS_BUCKET,
+  aws_s3_domain: process.env.AWS_S3_DOMAIN,
   port: process.env.PORT,
 };
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import AWS from 'aws-sdk';
 
 import VideoController from '../controllers/video';
 
@@ -12,7 +13,59 @@ export default (app) => {
   app.route('/video/:key')
     .get((req, res) => {
       const { key } = req.params;
-      const movieFile = `/${app.config.aws_bucket}/${key}`;
+
+      AWS.config.update({
+        accessKeyId: app.config.aws_key,
+        secretAccessKey: app.config.aws_secret,
+      });
+
+      const s3 = new AWS.S3();
+      const file = fs.createWriteStream(`./streaming/${key}`);
+
+      const params = {
+        Bucket: app.config.aws_bucket,
+        Key: `processed/${key}`,
+      };
+
+      s3.getObject(params).createReadStream().pipe(file)
+        .on('finish', () => {
+          const filePath = `./streaming/${key}`;
+
+          fs.stat(filePath, (err, stats) => {
+            if (err) {
+              return res.status(404).json(err);
+            }
+            // Setup the chunk headers variables
+            const { range } = req.headers;
+            const { size } = stats;
+            const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
+            const end = size - 1;
+            const chunkSize = (end - start) + 1;
+            // Set chunk headers
+            res.set({
+              'Content-Range': `bytes ${start}-${end}/${size}`,
+              'Accept-Ranges': 'bytes',
+              'Content-Length': chunkSize,
+              'Content-Type': 'video/mp4',
+            });
+
+            // Read the file and send chunks through stream.pipe()
+            const stream = fs.createReadStream(filePath, { start, end });
+            stream.on('open', () => stream.pipe(res));
+            stream.on('error', streamErr => res.end(streamErr));
+
+            // Send a PARTIAL CONTENT status code
+            return res.status(206);
+          });
+        });
+    });
+
+  app.route('/old/video/:key')
+    .get((req, res) => {
+      const { key } = req.params;
+      const movieFile = `${app.config.aws_bucket}/processed/${key}`;
+
+      console.log(movieFile);
 
       fs.stat(movieFile, (err, stats) => {
         if (err) {
