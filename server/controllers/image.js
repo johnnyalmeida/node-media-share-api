4d585f90907c9bf2d52a51bcc1702ace22d39b31
Johnny M. Almeida <johnny.almeida@4all.com>
2018-06-12 12:01:23 -0300

    Process image, remove streaming folder

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 import uuid from 'uuid';
 import path from 'path';
 import formidable from 'formidable';
+import Jimp from 'jimp';
 
 /**
  * Default success response callback
@@ -53,22 +54,86 @@ class ImageController {
           const s3 = new AWS.S3();
           const bucket = this.config.aws_bucket;
           const token = uuid();
-          const key = `images/${token}${path.extname(files.file.name)}`;
+          const key = `${token}${path.extname(files.file.name)}`;
 
           s3.putObject({
             Bucket: bucket,
-            Key: key,
+            Key: `images/${key}`,
             Body: base64data,
-          }, (errorS3, result) => {
+          }, (errorS3) => {
             if (errorS3) {
               reject(errorS3);
             }
-            const response = {
-              path: key,
-              s3: result,
-            };
-            resolve(defaultResponse(response));
+            // const response = {
+            //   path: key,
+            //   s3: result,
+            // };
+            this.processImage(key)
+              .then((response) => {
+                resolve(defaultResponse(response));
+              })
+              .catch(processError => reject(processError));
           });
+        });
+      });
+    });
+  }
+
+  processImage(key) {
+    return new Promise((resolve, reject) => {
+      const file = fs.createWriteStream(`./tmp/images/${key}`);
+
+      const params = {
+        Bucket: this.config.aws_bucket,
+        Key: `images/${key}`,
+      };
+
+      this.s3.getObject(params).createReadStream().pipe(file)
+        .on('finish', () => {
+          const filePath = `./tmp/images/${key}`;
+          // console.log(filePath);
+          Jimp.read(filePath).then((image) => {
+            image
+              .exifRotate()
+              .scaleToFit(1080, 1920)
+              .quality(75)
+              .write(`./tmp/images/processed/${key}`, () => {
+                this.moveImageToS3(key, filePath)
+                  .then(data => resolve(data))
+                  .catch(err => reject(err));
+              });
+          }).catch((err) => {
+            reject(err);
+          });
+        });
+    });
+  }
+
+  /**
+   * Move processed image to S3
+   */
+  moveImageToS3(fileName, filePath) {
+    return new Promise((resolve, reject) => {
+      // Read in the file, convert it to base64, store to S3
+      fs.readFile(filePath, (err, data) => {
+        if (err) { throw err; }
+        const base64data = Buffer.from(data, 'binary');
+        const bucket = this.config.aws_bucket;
+        const key = `images/processed/${fileName}`;
+
+        this.s3.putObject({
+          Bucket: bucket,
+          Key: key,
+          Body: base64data,
+        }, (error, result) => {
+          if (error) {
+            reject(error);
+          }
+          const file = {
+            path: key,
+            s3: result,
+          };
+          resolve(file);
         });
       });
     });
@@ -82,7 +147,7 @@ class ImageController {
 
     const params = {
       Bucket: this.config.aws_bucket,
-      Key: `images/${key}`,
+      Key: `images/processed/${key}`,
     };
 
     this.s3.getObject(params)
